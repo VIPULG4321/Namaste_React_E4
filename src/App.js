c348c98dc3c875a68478b9f5bc7f40527e9266c5
VIPULG4321 <vipulgupta611@gmail.com>
2024-02-28 09:19:50 +0530

    namsate react L6 1/3

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import Header from "./components/Header";
-import Search from './components/Search';
-import Foods from './components/Foods';
-import './App.css';
-import React, { useState } from 'react';
+import Search from "./components/Search";
+import Foods from "./components/Foods";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 import initialRestourantData from "./utils/restourant";
 
 function App() {
-  
-  const [restourantData, setRestourantData] = useState(initialRestourantData); 
+  const [restourantData, setRestourantData] = useState(initialRestourantData);
   
   const handleFilterClick = () => {
-    const newFilteredData = initialRestourantData.filter((item) => item.info.avgRating > 4);
-    setRestourantData(newFilteredData);
-  };
+    const newFilteredData = initialRestourantData.filter(
+      (item) => item.info.avgRating > 4
+      );
+      setRestourantData(newFilteredData);
+    };
+    
+    const handle_Back = () => {
+      const filteredData = initialRestourantData.map((item) => item);
+      setRestourantData(filteredData);
+    };
+    
+    useEffect(() => {
+      fetchData();
+    }, []);
 
-  const handle_Back = () => {
-    const filteredData = initialRestourantData.map((item)=> item);
-    setRestourantData(filteredData);
-  }
+    const fetchData = async () => {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+      const json = await data.json();
+      console.log(json); 
+
+    }
 
   return (
     <div className="App">
       <div id="header_container">
         <Header />
         <Search></Search>
-        <button
-        className="filter"
-        onClick={handleFilterClick}
-        >
-        Top rated - restaurants
+        <button className="filter" onClick={handleFilterClick}>
+          Top rated - restaurants
         </button>
-        <button onClick={handle_Back}>Back</button>
-        <div id='items'>
-          {
-            restourantData.map((restourant, index)=>(
-              <Foods key={index} RestData={restourant}/>
-            ))
-          }
+
+        <button className="back" onClick={handle_Back}>
+          Back
+        </button>
+        <div id="items">
+          {restourantData.map((restourant, index) => (
+            <Foods key={index} RestData={restourant} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,3 @@
-// import restourantData from "../utils/restourant";
-
-// export var modified_res_data = [];
-// modified_res_data = restourantData;
 
 const search_style = {
   color: "red",
@@ -18,7 +14,7 @@ const Search = function () {
   return (
     <div className="search">
       <input type="text"></input>
-      <button>Search</button>
+      <button style={btn_style}>Search</button>
       <p style={search_style}>Please select pincode here..</p>
     </div>
   );
