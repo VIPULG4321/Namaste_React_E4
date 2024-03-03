716998fb017f701ad7a7319c77f7108a43c693cd
VIPULG4321 <vipulgupta611@gmail.com>
2024-03-03 23:56:18 +0530

    namsate react L6 3/3 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 import Foods from "./components/Foods";
 import "./App.css";
 import React, { useState, useEffect } from "react";
@@ -7,11 +7,15 @@ import Shimmer from "./components/shimmerUI";
 
 function App() {
   const [restourantData, setRestourantData] = useState([]);
+  const [searchValue, setsearchValue] = useState("");
+  const [filteredRestourants, setfilteredRestourants] = useState([]);
+
 
   
   const handleFilterClick = () => { 
+
     const newFilteredData = restourantData.filter(
-      (item) => item.info.avgRating > 4
+      (item) => item.info.avgRating > 4.4
       );
       setRestourantData(newFilteredData);
     };
@@ -22,12 +26,19 @@ function App() {
 
     const fetchData = async () => {
       const data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.145269550486525&lng=72.83064298331738&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
+      // const data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9921457&lng=72.846563&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTIN`);
+
+      // https:/corsproxy.io/
 
       const json = await data.json();
+      console.log(json);
 
       const {restaurants} = json?.data.cards[1].card.card.gridElements.infoWithStyle; //optional chaining.
       
-      setRestourantData(restaurants); //setRestourantData() is a setstate function which setting the state of state variable.
+      setRestourantData(restaurants);
+      setfilteredRestourants(restaurants);
+      
+      //setRestourantData() is a setstate function which setting the state of state variable.
       
     }
 
@@ -35,13 +46,28 @@ function App() {
     <div className="App">
       <div id="header_container">
         <Header />
-        <Search></Search>
-        <button className="filter" onClick={handleFilterClick}>
-          Top rated - restaurants
-        </button>
+        {/* <Search></Search> */}
+
+        <div className="Search">
+          <input type="email" id="search-box" value={searchValue} onChange={(e)=>{setsearchValue(e.target.value)}}></input>
+          <button onClick={()=>{
+
+            const filteredData =  restourantData.filter((res)=>{
+              return res.info.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            setfilteredRestourants(filteredData);
+            
+          }}>Search</button>
+
+          <button className="filter" onClick={handleFilterClick}>
+            Top rated - restaurants
+          </button>
+        </div>
+
+        
 
         <div id="items">
-          {restourantData.map((restourant, index) => (
+          {filteredRestourants.map((restourant, index) => (
             <Foods key={index} RestData={restourant} /> 
           ))}
         </div>
--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,22 +1,30 @@
 import { CDN_IMG } from "../utils/constants";
+// import { Img } from 'react-image';
+import star from "../utils/star.jpg";
 
 const Foods = (props) => {
-    const {RestData} = props;
-    const {cloudinaryImageId, name, cuisines, avgRating, locality, areaName} = RestData?.info;
+    const {RestData} = props; //object destructuring
+    const {cloudinaryImageId, name, cuisines, avgRating, locality, areaName, costForTwo} = RestData?.info; //optional chaining.
+    var mycus = cuisines.join(", ").length;
     
     return (
       <div className='card'>
         <div className='card_img'>
           <img src={CDN_IMG + cloudinaryImageId } alt='mydata'></img>
+          <h1 className="costfortwo">{costForTwo}</h1>
         </div>
         <div className='class_details'>
-          <p className='name'>{name}</p>
-          <p className='cuisines'><span>{cuisines.join(", ")}</span></p>
-          <p>{avgRating} Stars • 50-55 min</p>
-          <p>{locality}, {areaName}</p>
+          <h3 className='res-name'>{name}</h3>
+          <p>{avgRating } Stars • 50-55 min</p>
+          {
+            (mycus>55) ? <p className='cuisines'><span>{cuisines.join(", ").substring(0,55)}..</span></p>
+             : <p className='cuisines'><span>{cuisines.join(", ")}</span></p>
+          } 
+          <p className="loc">{locality}, {areaName}</p>
         </div>
       </div>
     )
+    
   }
 
   export default Foods;
